a17a20644c230796b0ffac241e6230d1d3cd2450
Nawras-Mansour <manssour.nawras@gmail.com>
2025-01-14 12:41:20 +0400

    fix config params

--- src/config/auth.config.ts
+++ src/config/auth.config.ts
@@ -1,4 +1,4 @@
 export const authConfig = () => ({
     JWT_SECRET: process.env.JWT_SECRET || 'tester',
-    JWT_EXPIRATION : process.env.DATABASEJWT_EXPIRATION_DB_LOCAL || 3600,
+    JWT_EXPIRATION : process.env.JWT_EXPIRATION_DB_LOCAL || 3600,
 });
--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -1,8 +1,8 @@
 export const databaseConfig = () => ({
-    DB_HOST : process.env.MYSQL_HOST || 'localhost',
-    DB_PORT : process.env.MYSQL_PORT || 3306,
-    DB_NAME : process.env.MYSQL_DATABASE || 'nest_tester',
-    DB_USER : process.env.MYSQL_USERNAME || 'root',
-    DB_PASS : process.env.MYSQL_PASSWORD || '',
-    DB_SYNC : process.env.MYSQL_SYNCHRONIZE || true,
+    DB_HOST        : process.env.DB_HOST || 'localhost',
+    DB_PORT        : process.env.DB_PORT || 3306,
+    DB_DATABASE    : process.env.DB_DATABASE || 'nest_tester',
+    DB_USERNAME    : process.env.DB_USERNAME || 'root',
+    DB_PASSWORD    : process.env.DB_PASSWORD || '',
+    DB_SYNCHRONIZE : process.env.DB_SYNCHRONIZE || true,
 });
